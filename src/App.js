53e0f3d5ea406b32426462d493524392d7cc5b2b
Weronika Kansy <weronika.kansy@gmail.com>
2022-04-01 00:25:57 +0200

    Mode saved in state and toggled with an onclick handler; default/light styles applied based on the App container className which changes with each state update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState("true");
+
+  function handleToggle() {
+    setDarkMode((prevMode) => !prevMode);
+  }
+
   return (
-    <div className="container">
-      <Header />
+    <div className={darkMode ? "container" : "light container"}>
+      <Header toggle={handleToggle} />
       <Main />
       <Footer />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import logo from "../assets/reactjs-icon.svg";
 
-function Header() {
+function Header({ toggle }) {
   return (
     <header>
       <nav className="nav">
@@ -8,7 +8,7 @@ function Header() {
         <h3 className="nav__logo_text">ReactFacts</h3>
         <div className="nav__toggler">
           <p className="toggler__text--light">Light</p>
-          <div className="toggler__slider">
+          <div className="toggler__slider" onClick={toggle}>
             <div className="toggler__slider__circle"></div>
           </div>
           <p className="toggler__text--dark">Dark</p>
